45cc6a06fd56cfa20c1f15d4f9c9da1664c2f38f
rgaitan21 <rgaitan@unah.hn>
2024-07-13 00:22:41 -0600

    Add files via upload

---
+++ Sumatoria/src/main/java/com/mycompany/sumatoria/Sumatoria.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sumatoria;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Sumatoria {
+
+    public static void main(String[] args) {
+        Sumar suma1 = new Sumar();
+        
+        suma1.setVisible(true);
+        
+    }
+}
